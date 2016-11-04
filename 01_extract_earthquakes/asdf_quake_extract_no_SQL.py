0bdca756abe5e8e336427b51f7ff0b8148bd5e94
outdoorpet <acooper8@utas.edu.au>
2016-11-04 13:23:55 +1100

    finished writing no SQL earthquake extractor

--- 01_extract_earthquakes/asdf_quake_extract_no_SQL.py
+++ 01_extract_earthquakes/asdf_quake_extract_no_SQL.py
@@ -3,18 +3,20 @@
 
 import pyasdf
 from os.path import join
-from obspy.core import Stream
+import time
+
+code_start_time = time.time()
 
 # =========================== User Input Required =========================== #
 
 #Path to the data
-data_path = '/media/obsuser/seismic_data_1/'
+data_path = '/media/obsuser/GA-ANU_TRAN/'
 
 #IRIS Virtual Ntework name
-virt_net = '_GA_test'
+virt_net = '_ANU'
 
 # FDSN network identifier (2 Characters)
-FDSNnetwork = 'XX'
+FDSNnetwork = 'S1'
 
 # =========================================================================== #
 
@@ -29,6 +31,8 @@ print ds
 # Access the event metadata
 event_cat = ds.events
 
+files_added = 0
+
 for _j, event in enumerate(event_cat):
     #print '\r  Extracting {0} of {1} Earthquakes....'.format(_j + 1, event_cat.count()),
     #sys.stdout.flush()
@@ -41,12 +45,31 @@ for _j, event in enumerate(event_cat):
     print '...'
     print 'qtime = ', origin_info.time, qtime
 
-    for station in ds.ifilter(ds.q.starttime <= [qtime, qtime+3600]):#, qtime < ds.q.endtime, ds.q.tag == "raw_recording"):
-        print station
+    event_latitude = origin_info.latitude
+    event_longitude = origin_info.longitude
+    event_depth = origin_info.depth
+
+    #extract 30 mins before and 2 hours after earthquake origin time
+    st = ds.get_waveforms(network=FDSNnetwork, station='*', location='*', channel='*',
+                          starttime=origin_info.time-1750, endtime=origin_info.time+2*3600,
+                          tag='mondo_raw_recording')
+
+    if not st.__nonzero__():
+        continue
+
+    # add the st back into ASDF file referenced to the quake
+    ds.add_waveforms(st, tag='extracted_unproc_quakes', event_id=event)
+
+    files_added += 1
 
 
     break
 
+
+del ds
+print '\n'
+print("--- Execution time: %s seconds ---" % (time.time() - code_start_time))
+print '--- Added ', files_added, ' earthquake waveforms to ASDF file ---'
 
 
 
